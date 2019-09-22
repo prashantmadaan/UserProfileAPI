4274c134bcadb218fb5d86f73a64a680a8d7a9c3
prashantmadaan <prashantmadaan123@gmail.com>
2019-09-22 14:13:03 -0400

    Renaming files and references

--- server.js
+++ server.js
@@ -2,19 +2,19 @@ var express = require('express'),
   app = express(),
   port = process.env.PORT || 3000,
   mongoose = require('mongoose'),
-  Task = require('./api/models/todoListModel'), //created model loading here
+  Task = require('./api/models/userProfileAPIModel'), //created model loading here
   bodyParser = require('body-parser');
 
 // mongoose instance connection url connection
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/Tododb');
+mongoose.connect('mongodb://localhost/UserProfiledb');
 
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 
-var routes = require('./api/routes/todoListRoutes'); //importing route
+var routes = require('./api/routes/userProfileAPIRoutes'); //importing route
 routes(app); //register the route
 
 
